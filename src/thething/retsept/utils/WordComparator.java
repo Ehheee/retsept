c4d65088b58586f7cb42211020b43dfcb81dbfe8
Kaur Kase <ehheee@gmail.com>
2014-11-07 16:36:50 +0200

    More improvements

--- src/thething/retsept/utils/WordComparator.java
+++ src/thething/retsept/utils/WordComparator.java
@@ -5,51 +5,7 @@ import java.util.LinkedList;
 
 public class WordComparator {
 
-	
-	public double compareWords(String a, String b){
-		int basePoints = a.length() -1;
-		double totalPoints = 0;
-		System.out.println("basePoints set to: " + basePoints);
-		System.out.println("Evaluating a: " + a + " b: " + b);
-		for(int i = 0; i < basePoints +1; i++){
-			char ai = a.charAt(i);
-			int posInB = b.indexOf(ai);
-			
-			totalPoints += basePoints - Math.abs((i - posInB));
-			System.out.println("Evaluated char: " + ai + " index in b: " + posInB +  " posDifference: " + Math.abs((i - posInB)) + " newTotal: " + totalPoints);
-		}
-		totalPoints = totalPoints/(a.length() *b.length());
-		System.out.println("dividedTotal: " + totalPoints);
-		return totalPoints;
-	}
-	
-	public double compareWords2(String a, String b){
-		a = a.toLowerCase();
-		b = b.toLowerCase();
-		int basePoints = 3;
-		double totalPoints = 0;
-		System.out.println("Evaluating a: " + a + " b: " + b);
-		char[] bChars = b.toCharArray();
-		
-		int pushK = 0;
-		for(int i = 0; i < a.length() ; i++){
-			char ai = a.charAt(i);
-			for(int k = Math.max((i - (basePoints - 1)) + pushK, 0); k < (i+basePoints) && k < b.length(); k++){
-				if(a.charAt(i) == b.charAt(k)){
-					
-					totalPoints += basePoints - Math.abs(i - k + pushK);
-					
-					System.out.println("Index in a: " + i + " Index in b: " + k + " Points given: " + (basePoints - (Math.abs(i-k + pushK))));
-					if(i != k){
-						pushK = k -i;
-					}
-				}
-			}
-		}
-		totalPoints = totalPoints/a.length();
-		System.out.println("dividedTotal: " + totalPoints);
-		return totalPoints;
-	}
+
 	
 	public double compareWords3(String a, String b){
 		LinkedList<Character> aChars = this.wordToLinkedList(a);
@@ -83,28 +39,51 @@ public class WordComparator {
 			
 			}
 			
-			//else if character in string a exists in b but on the next position
-			else if(diff == 1 || diff == -1){
+			//else if character in string a exists in b but on the next position or doesn't exist at all i.e a letter is missed during typing
+			else if((diff == 1 || diff == -1) && (!aChars.isEmpty() && !bChars.isEmpty())){
 				
 				//If the next  char in a is at the current index in b, characters might be switched.
-				if(aChars.get(1) == bChars.get(0)){
+				if(aChars.size() > 1 && (aChars.get(1) == bChars.get(0))){
 					//If the next characters after the possibly switched ones match, there is high chance of spelling mistake
 					if(aChars.get(2) == bChars.get(2)){
-						System.out.println("Possibly switched characters: " + aChars.get(ai) + " , " +bChars.get(ai +1));
+						System.out.println("Possibly switched characters: " + aChar + " , " +bChars.get(1));
+						
 						totalPoints += 1;
+						//remove the first 2 characters as they are both evaluated and possible points given
+						
+						aChars.remove();
+						aChars.remove();
+						bChars.remove();
+						bChars.remove();
+						ai++;
 						
 					}
-					//remove the first 2 characters as they are both evaluated and possible points given
-					aChars.remove();
-					aChars.remove();
-					bChars.remove();
-					bChars.remove();	
+					//bChars might possibly miss a character
+					else if(bChars.get(1) == aChars.get(2)){
+						
+						System.out.println("bChars seems to miss a character: " + aChar);
+						totalPoints += 0.75;
+						aChars.remove();
+						aChars.remove();
+						bChars.remove();
+						ai++;
+					}
+					
+					else{
+						totalPoints +=0.5;
+						aChars.remove();
+						bChars.remove();
+					}
+					
+					
+					
 				}
 				
 				//else if the char in a is the next char in b, b might have an extra character
-				else if(aChar == bChars.get(1)){
+				else if(bChars.size() > 1 && (aChar == bChars.get(1))){
 					//next chars after that are the same
 					if(aChars.get(1) == bChars.get(2)){
+						System.out.println("bChars seems to have an extra character: " + aChar);
 						totalPoints += 0.25;
 						bChars.remove();
 					}
@@ -115,29 +94,28 @@ public class WordComparator {
 					
 				}
 				
-				//else if the current char in b equals the next one in a, b might miss a character
-				else if(bChars.get(0) == aChars.get(1)){
-					//next chars after that are the same
-					if(bChars.get(1) == aChars.get(2)){
-						totalPoints += 0.75;
+				else{
+					if(aChars.size() > 1){
+						aChars.remove();
 					}
-					aChars.remove();
-					aChars.remove();
-					bChars.remove();
-					
-				}else{
-					aChars.remove();
-					bChars.remove();
+					if(bChars.size() > 1){
+						bChars.remove();
+					}
 				}
 				
 			}else{
-				aChars.remove();
-				bChars.remove();
+				if(aChars.size() > 1){
+					aChars.remove();
+				}
+				if(bChars.size() > 1){
+					bChars.remove();
+				}
 			}
 			
 			System.out.println("New totalpoints: " + totalPoints);
 		}
 		System.out.println("Totalpoints: " + totalPoints);
+		System.out.println("Theoretical average: " + totalPoints/aSize);
 		return 0;
 	}
 	
